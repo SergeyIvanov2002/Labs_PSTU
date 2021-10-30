8fa3ea793c2f4ba433ae1ff5b6662773d5188f39
SergeyIvanov2002 <93069013+SergeyIvanov2002@users.noreply.github.com>
2021-10-30 13:47:38 +0500

    Add files via upload

---
+++ 1.1.pas
@@ -0,0 +1,24 @@
+﻿var n,a,b,c:integer;
+begin
+  while (a=b) or (b=c) or (a=c) or (a>9) or (b>9) or (c>9) or (n<10) do begin
+    writeln('Введите трёхзначное число с различными цифрами');
+  readln(n);
+ a:= n mod 10;
+ n:= n div 10;
+ b:= n mod 10;
+ c:= n div 10;
+ end;  
+ if (a>b) and (a>c) and (c>b) then 
+   write (b,c,a,' ',b,a,c,' ',c,b,a,' ',c,a,b,' ',a,b,c,' ',a,c,b,' ',a,c,b); 
+ if (a>b) and (a>c) and (c<b) then 
+   write (c,b,a,' ',c,a,b,' ',b,c,a,' ',b,a,c,' ',a,c,b,' ',a,b,c,' ',a,b,c);
+ if (a>b) and (a<c) and (c>b) then 
+   write (b,a,c,' ',b,a,c,' ',c,b,a,' ',c,a,b,' ',a,b,c,' ',a,c,b,' ',c,a,b);
+ if (a<b) and (a<c) and (c>b) then 
+   write (b,c,a,' ',b,a,c,' ',c,b,a,' ',c,a,b,' ',a,b,c,' ',a,c,b,' ',c,b,a);
+ if (a<b) and (a>c) and (c<b) then 
+   write (b,c,a,' ',b,a,c,' ',c,b,a,' ',c,a,b,' ',a,b,c,' ',a,c,b,' ',b,a,c);
+ if (a<b) and (a<c) and (c<b) then 
+ write (b,c,a,' ',b,a,c,' ',c,b,a,' ',c,a,b,' ',a,b,c,' ',a,c,b,' ',b,c,a);
+end.
+
---
+++ 1.2.pas
@@ -0,0 +1,15 @@
+﻿var a,n,k:integer;
+begin
+  k:=8;
+  a:=random(100)+1;
+  writeln('Угадайте число');
+  while (a<>n) and (k>0) do begin
+  readln(n);
+  if a>n then writeln ('Больше');
+  if a<n then writeln ('Меньше');
+  if a=n then writeln ('Вы победили');
+  k:=k-1;
+  end;
+  if a=n then k:=k+1;
+  if k=0 then write('Вы проиграли');
+end.
